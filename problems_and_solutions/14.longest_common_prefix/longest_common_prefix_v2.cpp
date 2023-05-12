a895db6303d89252f9ebec8110ae61b312243288
n8fury <mhjoy547@yahoo.com>
2023-05-12 19:49:57 +0600

    added optimized solution for problem#14

---
+++ problems_and_solutions/14.longest_common_prefix/longest_common_prefix_v2.cpp
@@ -0,0 +1,23 @@
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) {
+        return "";
+    }
+    
+    string prefix = strs[0];
+    
+    for (int i = 1; i < strs.size(); i++) {
+        int j = 0;
+        while (j < prefix.length() && prefix[j] == strs[i][j]) {
+            j++;
+        }
+        
+        prefix = prefix.substr(0, j);
+        
+        if (prefix.empty()) {
+            return "";
+        }
+    }
+    
+    return prefix;
+    
+}
