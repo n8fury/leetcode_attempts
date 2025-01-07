33f174fe2d4c2cadb0df80a7a8419bc1a1d7ba5e
n8fury <mhjoy547@yahoo.com>
2025-01-07 13:09:31 +0600

    added solve for 1408

---
+++ problems_and_solutions/1408. String Matching in an Array/1408. String Matching in an Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> result;
+        int n = words.size();
+
+//  Compare every word with every other word
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    result.push_back(words[i]);
+                    break; // No need to check further once found
+                }
+            }
+        }
+        
+        return result;
+    }
+};
